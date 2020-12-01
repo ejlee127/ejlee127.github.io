1af38bb92a6a8890289570ac760779372953894f
Lyra <63160185+ejlee127@users.noreply.github.com>
2020-11-30 23:51:23 -0500

    adding detail.js

---
+++ static/js/detail.js
@@ -0,0 +1,99 @@
+var journal_publications = [
+    "Hoon Hong, Eunjeong Lee and Hyang-Sook Lee, Explicit formula for optimal ate pairing over cyclotomic family of elliptic curves, Finite Fields and Their Applications, Volume 34, pages 45- 74 (2015)",
+    "Seongan Lim, Eunjeong Lee and Cheol-Min Park, Equivalent public keys and a key substitu- tion attack on the schemes from vector decomposition, Security and Communication Networks Volume 7, Issue 8, pages 1274-1282 (2014)",
+    "Eunjeong Lee, Hyang-Sook Lee and Cheol-Min Park, Polynomial generating pairing and its criterion for optimal pairing, Applicable Algebra in Engineering, Communication and Computing, Volume 25, Issue 4, pages 227-247 (2014)",
+    "Soo Kyung Eom, Eunjeong Lee, and Hyang-Sook Lee, Faster Ate Pairing Computation over Pairing-Friendly Elliptic Curves Using GLV Decomposition, ETRI Journal, Volume 35, No. 5, pages 880-888 (2013)",
+    "Hoon Hong, Eunjeong Lee, Hyang-Sook Lee and Cheol-Min Park, Simple and exact formula for minimum loop length in Atei pairing based on Brezing-Weng curves, Designs, Codes and Cryptography, Volume 67, Issue 2, pages 271-292 (2013)",
+    "Hoon Hong, Eunjeong Lee, Hyang-Sook Lee and Cheol-Min Park, Maximum gap in (inverse) cyclotomic polynomial, Journal of Number Theory, Volume 132, Issue 10, pages 2297-2315 (2012)",
+    "Seongan Lim, Eunjeong Lee and Cheol-Min Park, A short redactable signature scheme using pairing, Security and Communication Networks, Volume 5, Issue 5, pages 523-534 (2012)",
+    "Eunjeong Lee, Hyang-Sook Lee and Cheol-Min Park, Efficient and Generalized Pairing Com- putation on Abelian Varieties, IEEE Transactions on Information Theory, Volume 55, Issue 4, pages 1793-1803 (2009)",
+    "Eunjeong Lee, Hyang-Sook Lee and Yoonjin Lee, Eta pairing computation on general divisors over hyperelliptic curves y2 = xp −x±1, Journal of Symbolic Computation, Volume 43, pages 452-474 (2008)",
+    "Eunjeong Lee and Yoonjin Lee, Tate pairing computation on the divisors of hyperelliptic curves of genus 2, Journal of the Korean Mathematical Society , Volume 45, No. 4, pages 1057-1073 (2008)",
+    "YoungJu Choie, Eunkyung Jeong and Eunjeong Lee, Supersingular hyperelliptic curves of genus 2 over finite fields, Applied Mathematics and Computation, Volume 163, No.2, pages 565-576 (2005)",
+    "YoungJu Choie, Eunkyung Jeong and Eunjeong Lee, Efficient identity-based authenticated key agreement protocol from pairings, Applied Mathematics and Computation, Volume 162, No.1, pages 179-188, (2005)"
+];
+
+var coferenece_publications =[
+    "Seunghwan Chang, Hoon Hong, Eunjeong Lee and Hyang-Sook Lee, Pairing Inversion via Non-degenerate Auxiliary Pairings, Pairing-Based Cryptography-Pairing 2013, Peijing, Lecture Notes in Computer Science, Volume 8365, pages 77-96 (2014)",
+    "Eunjeong Lee, Hyang-Sook Lee and Yoonjin Lee, Eta pairing computation on general divisors over hyperelliptic curves y^2=x^7-x+/- 1, Extended abstract in the Proceedings of Pairing-Based Cryptography-Pairing 2007, 2007.7.2-4, Tokyo, Lecture Notes in Computer Science, Volume 4575, Springer, Berlin, pages 349-366 (2007)",
+    "YoungJu Choie, and Eunjeong Lee, Implementation of Tate Pairing of Hyperelliptic Curves of Genus 2, Proceedings of ICISC 2003, Lecture Notes in Computer Science, Volume 2971, Springer, Berlin, pages 97-111 (2004.5.12 Online date)"
+];
+
+var submitted_articles = [
+    "Ala'a Al-Kateeb, Mary Ambrosino, Hoon Hong, and Eunjeong Lee, Maximum gap in cyclotomic polynomials, arXiv:1911.11667v3, submitted to Journal of Number Theory, 2019",
+    "Mary Ambrosino, Hoon Hong, and Eunjeong Lee, Lower bounds for maximum gap in (inverse) cyclotomic polyno- mials. Technical report, arxiv.org/abs/1702.07650, 2017",
+    "Ala'a Al-Kateeb, Hoon Hong, and Eunjeong Lee, Block structure of cyclotomic polynomials. Technical report, arXiv:1704.04051v2, 2017"
+];
+
+var talks_list = [
+    "Optimal generalized ate pairing over cyclotomic family of elliptic curve, Dept. of Math. (symbolic computation group), North Carolina State University, April 21, 2015",
+    "Introduction to Cryptography for undergraduate students, Dept. of Physics, Korea Univer- sity, Seoul, Korea, Dec. 10, 2013",
+    "Pairing Inversion via Non-degenerate Auxiliary Pairings, Pairing-Bassed Cryptography-Pairing 2013 Conference, Peijing, China, Nov. 22-24, 2013",
+    "Reducing Pairing Inversion to Exponentiation Inversion using Non-degenerate Auxiliary Pair- ings, The Asian Mathematical Conference, Pusan, Korea, July 2, 2013",
+    "Simple and exact formula for minimum loop length in Ate i pairing based on Brezing Weng curves, International Conference on Coding and Cryptography, Seoul, Korea, August 25, 2012",
+    "Pairing for Cryptography, Dept. of Mathematics, University of North Carolina at Greensboro, Nov. 13, 2009",
+    "'Curious' graphs arising from cyclotomic polynomials, Dept. of Mathematics (symbolic computation group), North Carolina State University, Oct. 28, 2009",
+    "Pairing for Cryptography, Dept. of Mathematics (symbolic computation group), North Car- olina State University, Sep. 30, 2009"
+];
+
+var grants_list = [
+    "Basic Science Research Program (Grant No. 2011-0022600) Title: Sparsity of cyclotomic polynomials and its application to efficient pairings (2011.09.01 - 2014.08.31)",
+    "Co-PI of the grant supported by KOSEF (Grant No. R01-2005-000-10713-0 ) Title: Design and analysis of cryptosystem using bilinear maps (2005.04.01 - 2008.03.31)"
+];
+
+var service_referee = [
+    "Proceeding of AsiaCrypt 2015, November 29-December 3, 2015, Auckland, New Zealand",
+    "Proceeding of ICISC 2014, December 25-27, 2014, Seoul, Korea",
+    "Journal of Security and Communication Networks (2013.12)",
+    "Proceeding of ICISC 2012, November 28-30, 2012, Seoul, Korea",
+    "Journal of Security and Communication Networks (2010.12)",
+    "Journal of Systems and Software, Elsevier (2009.02)",
+    "Proceeding of ICISC 2008, December 3-5, 2008, Seoul, Korea",
+    "Proceeding of Cryptographic Hardware and Embedded Systems 2007 (2007.01)",
+    "Human-Tech Thesis Prize 2007 organized by Samsung Electronics (2007. 01)",
+    "Journal of Korea Information Processing Society (2006.06 and 2007.02)",
+    "Cryptology Thesis Prize organized by NIS, Korea (2006.05)"
+];
+
+var teachings_list = [
+    "Introduction to Cryptography: Fall 2010, Fall 2011, Fall 2012",
+    "Algorithms for Cryptography (graduate course): Spring 2012, Fall 2013",
+    "Number Theory and Cryptography (graduate course): Spring 2011",
+    "Modern Cryptgraphy (graduate course): Spring 2010"
+];
+
+var reports = d3.select("#reports");
+submitted_articles.forEach( article => {
+    reports.append("li").text(article)
+});
+
+var refereed = d3.select("#refereed");
+journal_publications.forEach( paper => {
+    console.log(paper)
+    refereed.append("li").text(paper)
+});
+
+var conference = d3.select("#conference");
+coferenece_publications.forEach( paper => {
+    conference.append("li").text(paper)
+});
+
+var grants = d3.select("#grants");
+grants_list.forEach( grant => {
+    grants.append("li").text(grant)
+});
+
+var talks = d3.select("#talks");
+talks_list.forEach(talk => {
+    talks.append("li").text(talk)
+});
+
+var teachings = d3.select("#teachings");
+teachings_list.forEach(teaching => {
+    teachings.append("li").text(teaching)
+});
+
+var services = d3.select("#services");
+service_referee.forEach(referee => {
+    services.append("li").text(referee)
+});
